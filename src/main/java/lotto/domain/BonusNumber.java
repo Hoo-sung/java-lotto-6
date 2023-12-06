8e8d301f678160bc661a8c37a3a71d5f4c935f50
Hoo-sung <gntjd135@gmail.com>
2023-12-06 11:01:29 +0900

    feat(BonusNumber): 보너스 번호 도메인 생성

---
+++ src/main/java/lotto/domain/BonusNumber.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+
+import static lotto.system.ExceptionMessage.INVALID_BONUS_NUMBER;
+
+public class BonusNumber {
+
+
+    /**
+     * 보너스 번호가 유효한 범위의 숫자인지 검증해야 한다.
+     */
+    private final static int START_NUM = 1;
+    private final static int END_NUM = 45;
+    private final int bonusNumber;
+
+
+    public BonusNumber(int bonusNumber) {
+        validate(bonusNumber);
+        this.bonusNumber = bonusNumber;
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+
+    private void validate(int bonusNumber) {
+        validateNumberRange(bonusNumber);
+    }
+
+    private void validateNumberRange(int bonusNumber) {
+        if (bonusNumber < START_NUM || bonusNumber > END_NUM) {
+            throw new IllegalArgumentException(INVALID_BONUS_NUMBER);
+        }
+    }
+}
--- src/main/java/lotto/system/ExceptionMessage.java
+++ src/main/java/lotto/system/ExceptionMessage.java
@@ -3,4 +3,5 @@ package lotto.system;
 public class ExceptionMessage {
 
     public final static String INVALID_LOTTO = "[ERROR] 올바르지 않은 로또 입니다. 다시 입력해 주세요";
+    public final static String INVALID_BONUS_NUMBER = "[ERROR] 올바르지 않은 보너스 번호 입니다. 다시 입력해 주세요";
 }
