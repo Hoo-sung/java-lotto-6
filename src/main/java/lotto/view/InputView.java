ca874ee57006797b314f7f2e3400553bd18a23f2
Hoo-sung <gntjd135@gmail.com>
2023-12-06 14:44:35 +0900

    feat(InputView): InputView 클래스 생성

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,37 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static String MONEY_INPUT_MESSAGE = "구입 금액을 입력해주세요.";
+    private static String WINNING_LOTTO_INPUT_MESSAGE = "당첨 번호를 입력해 주세요.";
+    private static String BONUS_NUM_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public static InputView INPUT_VIEW = new InputView();
+
+    private InputView() {
+
+    }
+
+    public int readMoney() {
+        System.out.println(MONEY_INPUT_MESSAGE);
+        String money = Console.readLine();
+        //검증
+        return Integer.parseInt(money);
+    }
+
+    public String readWinningLotto() {
+        System.out.println(WINNING_LOTTO_INPUT_MESSAGE);
+        String winningLotto = Console.readLine();
+        //검증
+        return winningLotto;
+    }
+
+    public int readBonusNumber() {
+        System.out.println(BONUS_NUM_INPUT_MESSAGE);
+        String bonusNumber = Console.readLine();
+        //검증
+        return Integer.parseInt(bonusNumber);
+    }
+}
