9b0606bcd11bbbd23677ba2b271ef3953fcf5893
Hoo-sung <gntjd135@gmail.com>
2023-12-06 10:00:30 +0900

    feat(LottoRepository): LottoRepository 도메인 생성

---
+++ src/main/java/lotto/domain/repository/LottoRepository.java
@@ -0,0 +1,31 @@
+package lotto.domain.repository;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class LottoRepository {
+
+    /**
+     * 로또저장소이다. 로또저장소에 로또를 추가하는 기능
+     */
+
+    private final List<Lotto> lottoRepository;
+
+    public LottoRepository(List<Lotto> lottoRepository) {
+        this.lottoRepository = lottoRepository;
+    }
+
+    public void add(Lotto lotto) {
+        lottoRepository.add(lotto);
+    }
+
+    public Lotto get(int index) {
+        return lottoRepository.get(index);
+    }
+
+    public int size() {
+        return lottoRepository.size();
+    }
+
+}
